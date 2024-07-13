4b2ca79c37120ef37c8e53afd239499eecc7b668
Dharmik Patel <dharmikpatel2200dr@gmail.com>
2024-07-13 11:33:45 +0530

    sjhdjshdjshds

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
 
-      <div className="container mx-auto px-8">
+      <div className="container mx-auto px-9">
         <Navbar />
         <Hero />
         <About />
