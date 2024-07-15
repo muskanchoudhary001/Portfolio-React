5b2afab71c8436571af1b0504ce8af67efd70c25
Muskan Choudhary <81254978+muskanchoudhary001@users.noreply.github.com>
2024-07-15 13:17:05 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,8 +19,10 @@ const Contact = () => {
                 <motion.p
                     whileInView={{ opacity: 1, x: 0 }}
                     initial={{ opacity: 0, x: 100 }}
-                    transition={{ duration: 1 }} className="my-4">{CONTACT.phoneNo}</motion.p>
-                <a href="mailto:muskanchoudhary1303@gmail.com" className="border-b">{CONTACT.email}</a>
+                    transition={{ duration: 1 }} className="my-4"> 
+                    <a href="mailto:muskanchoudhary1303@gmail.com" className="border-b">{CONTACT.email}</a>
+                </motion.p>
+               
             </div>
         </div>
     )
