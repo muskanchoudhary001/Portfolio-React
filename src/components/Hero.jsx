5e435894e7e417c6ca1008c2ad54e1d67cdc82e0
Muskan Choudhary <81254978+muskanchoudhary001@users.noreply.github.com>
2024-10-08 15:05:26 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
             initial="hidden"
             animate="visible"
 
-            className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Muskan Choudhary</motion.h1>
+            className="pb-16 text-6xl font-bold tracking-tight lg:mt-16 lg:text-8xl">Muskan Choudhary</motion.h1>
           <motion.span
             variants={container(0.5)}
             initial="hidden"
