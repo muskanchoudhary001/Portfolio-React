f1f92d3339a4005bccd822eaf3852dd5488231aa
Muskan Choudhary <81254978+muskanchoudhary001@users.noreply.github.com>
2024-10-08 15:14:13 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,12 +16,14 @@ const Hero = () => {
     <div className="flex flex-wrap">
       <div className="w-full lg:w-1/2">
         <div className="flex flex-col items-center lg:items-start">
-          <motion.h1
-            variants={container(0)}
-            initial="hidden"
-            animate="visible"
+       <motion.h1
+  variants={container(0)}
+  initial="hidden"
+  animate="visible"
+  className="pb-16 text-6xl font-normal tracking-tight lg:mt-16 lg:text-8xl">
+  Muskan Choudhary
+</motion.h1>
 
-            className="pb-16 text-6xl font-bold tracking-tight lg:mt-16 lg:text-8xl">Muskan Choudhary</motion.h1>
           <motion.span
             variants={container(0.5)}
             initial="hidden"
