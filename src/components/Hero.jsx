9a76783bc9e5c30cafe805161b076365c21b0fef
Muskan Choudhary <81254978+muskanchoudhary001@users.noreply.github.com>
2025-02-04 12:13:46 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/1.jpg";
+import profilePic from "../assets/2.jpg";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
