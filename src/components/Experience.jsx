a37391ef15eceb4848f39f8094054c6b29414864
Muskan Choudhary <81254978+muskanchoudhary001@users.noreply.github.com>
2025-02-05 16:37:02 +0530

    updated responsiveness

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,32 +8,42 @@ const Experience = () => {
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: -100 }}
         transition={{ duration: 0.5 }}
-        className="my-20 text-center text-4xl"
+        className="my-20 text-center text-3xl md:text-4xl"
       >
         Experience
       </motion.h2>
       <div>
         {EXPERIENCES?.map((experience, index) => (
-          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+          <div
+            key={index}
+            className="mb-8 flex flex-col gap-4 lg:flex-row lg:justify-center"
+          >
+            {/* Left Column for Year */}
             <motion.div
               whileInView={{ opacity: 1, y: 0 }}
               initial={{ opacity: 0, x: -100 }}
               transition={{ duration: 1 }}
-              className="w-full lg:w-1/4"
+              className="w-full lg:w-1/4 text-center lg:text-left"
             >
-              <p className="mb-2 text-sm text-gray-400">{experience.year}</p>
+              <p className="text-sm text-gray-400">{experience.year}</p>
             </motion.div>
+
+            {/* Right Column for Role, Company, and Description */}
             <motion.div
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: 100 }}
               transition={{ duration: 1 }}
               className="w-full max-w-xl lg:w-3/4"
             >
-              <h6 className="mb-2 font-semibold text-gray-400">
-                {experience?.role} -
-                <span className="text-sm text-gray-400">{experience?.company}</span>
+              <h6 className="mb-2 font-semibold text-gray-400 text-center lg:text-left">
+                {experience?.role} -{" "}
+                <span className="text-sm text-gray-400">
+                  {experience?.company}
+                </span>
               </h6>
-              <p className="mb-4 text-neutral-300">{experience?.description}</p>
+              <p className="text-neutral-300 text-center lg:text-left">
+                {experience?.description}
+              </p>
             </motion.div>
           </div>
         ))}
