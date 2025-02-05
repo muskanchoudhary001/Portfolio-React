cd6821812476a13117e7c4b6d1272ce436143979
Muskan Choudhary <81254978+muskanchoudhary001@users.noreply.github.com>
2025-02-05 14:58:31 +0530

    Navbar Adjustments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="mb-14 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-10" src={logo} alt="logo" />
+        <img className="mx-2 w-14" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <a href="https://www.linkedin.com/in/muskan-choudhary-789759175/" target="_blank" rel='noopener noreferrer'>
